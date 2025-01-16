468a0930e4a9f3e43790c9e652c9ba1fc0e09fbb
asithniwantha <asithniwantha@gmail.com>
2025-01-16 14:13:08 +0530

    Update timer period for auto start and add version file

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from lib.ota_updater import OTAUpdater
 def auto_start():
     blink.blink_led()
 
-machine.Timer(-1).init(period=100, mode=machine.Timer.PERIODIC, callback=lambda t:auto_start())
+machine.Timer(-1).init(period=10, mode=machine.Timer.PERIODIC, callback=lambda t:auto_start())
 print("Auto start enabled")
 
 ota_updater = OTAUpdater(github_repo='https://github.com/asithniwantha/picoWifi')
 
-# Connect to WiFi and install update if available after boot
+# WiFi credentials
 ssid = 'linksys'
 password = 'asith1234567890'
+
+# Connect to WiFi and install update if available after boot
 if ota_updater.install_update_if_available_after_boot(ssid, password):
     print("Update installed. Rebooting...")
     machine.reset()
+
+
