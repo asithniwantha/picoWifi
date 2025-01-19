f5ef4cf0c1702602f5b958792941387d95cad93b
asithniwantha <asithniwantha@gmail.com>
2025-01-19 22:55:28 +0530

    Update version to 1.1 and add LED blinking functionality after OTA update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
+import blink
 
 firmware_url = "https://github.com/asithniwantha/picoWifi/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
 ota_updater.download_and_install_update_if_available()
+
+blink.blink_led()
